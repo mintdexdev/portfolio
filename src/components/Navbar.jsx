1cedd7e26d8e976c49ca1eb9007d8e2e6ed0c719
mintdexdev <dexdevpersona@gmail.com>
2025-09-08 07:00:00 +0530

    add navbar functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
 import ContainerX from './ui/ContainerX';
 import { navList } from '@/data/navList'
 
 export default function Navbar() {
+
+  const [lastScrollY, setLastScrollY] = useState(window.scrollY)
+  const [headerHideStyle, setHeaderHideStyle] = useState(null)
+
+  useEffect(() => {
+    const navigationHide = () => {
+      const scrollY = window.scrollY
+
+      if (scrollY === 0) {
+        setHeaderHideStyle('border-b border-white/0')
+      } else if (scrollY > lastScrollY) {
+        setHeaderHideStyle('translate-y-[-100%]')
+      }
+      else if (scrollY < lastScrollY) {
+        setHeaderHideStyle('border-b border-white/20')
+      }
+
+      setLastScrollY(scrollY)
+    }
+    window.addEventListener('scroll', navigationHide)
+    return () => window.removeEventListener('scroll', navigationHide)
+  }, [lastScrollY])
+
   return (
-    <nav className='w-full z-10'>
+    <nav className={`navbar fixed w-full z-10  duration-300 ${headerHideStyle}`}>
       <ContainerX>
 
         <div className='py-2 flex justify-between'>
@@ -15,7 +39,8 @@ export default function Navbar() {
 
           <ul className='flex gap-12'>
             {navList.map((item) => (
-              <li key={item.name}>
+              <li className=' will-change-transform duration-300 hover:brightness-75 hover:scale-[1.02] active:scale-[0.98] '
+                key={item.name} >
                 <a href={item.link}>{item.name}</a>
               </li>
             ))}
