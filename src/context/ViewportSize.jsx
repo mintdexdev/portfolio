9afc5ffa0dce8963148e03eddd4a5eda544535a4
mintdexdev <dexdevpersona@gmail.com>
2025-09-09 11:33:32 +0530

    add viewport context

---
+++ src/context/ViewportSize.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const ViewportSizeContext = createContext(null);
+
+export const useViewportSize = () => useContext(ViewportSizeContext)
+
+export const ViewportSizeProvider = ({ children }) => {
+
+  const getViewportSize = () => {
+    if (window.innerWidth < 640) return 'small';
+    if (window.innerWidth < 1024) return 'medium';
+    return 'large';
+  }
+
+  const [viewportSize, setViewportSize] = useState(getViewportSize())
+
+  useEffect(() => {
+    const handleResize = () => setViewportSize(getViewportSize());
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
+
+  return (
+    <ViewportSizeContext.Provider value={{ viewportSize }}>
+      {children}
+    </ViewportSizeContext.Provider>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './global.css'
-import App from './app/App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './global.css';
+import App from '@/app/App.jsx'
+import { ViewportSizeProvider } from '@/context/ViewportSize.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ViewportSizeProvider>
+      <App />
+    </ViewportSizeProvider>
   </StrictMode>,
-)
+);
